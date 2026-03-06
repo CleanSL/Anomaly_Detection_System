682a4fbbff9bb033a909ad0517d466ff1c6fe47d
harishprashanna <harishprashanna@icloud.com>
2026-03-06 15:13:04 +0530

    feat: add Residentd, Routelog, complaint and Driver classes to database setup

--- database_setup.py
+++ database_setup.py
@@ -7,3 +7,17 @@ DATABASE_URL = "sqlite:///./cleansl.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+#Tables
+
+class Resident(Base):
+    __tablename__ = "residents"
+    house_id = Column(String, primary_key=True)
+    road_id = Column(String, index=True)
+    past_violations = Column(Integer, default=0)
+    last_pickup_date = Column(DateTime)
+
+class Driver(Base):
+    __tablename__ = "drivers"
+    driver_id = Column(String, primary_key=True)
+    
